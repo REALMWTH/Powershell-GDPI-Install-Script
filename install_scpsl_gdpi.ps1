55a1216958014ca20b2cfa70bafacd2fbbe1a20f
REALM <realm_bmc@hotmail.com>
2024-03-19 03:36:46 +0300

    Update install_scpsl_gdpi.ps1

--- install_scpsl_gdpi.ps1
+++ install_scpsl_gdpi.ps1
@@ -5,7 +5,10 @@ function DeleteLeftoverFiles
 	)
 	
 	# Delete existing installation folder
-	if (Test-Path $old_folder_path) {[void](Remove-Item $old_folder_path -Recurse -Confirm:$False -Force)}
+	if (Test-Path $old_folder_path) {
+		Write-Output "Удаляем устаревшие файлы GoodbyeDPI"
+		[void](Remove-Item $old_folder_path -Recurse -Confirm:$False -Force)
+	}
 }
 
 # Set title and advertisement
@@ -41,12 +44,15 @@ else
 }
 
 # Checking if GoodbyeDPI is already installed. Ask to uninstall. If not accepted, exit script.
+Write-Output "Проверяем, установлен ли сервис WTH_GoodbyeDPI"
+
 $gdpi_service_exists = Get-Service -Name "WTH_GoodbyeDPI" -ErrorAction SilentlyContinue
 
 if ($gdpi_service_exists.Length -gt 0) {
 	$result = [System.Windows.Forms.MessageBox]::Show('Найден установленный ранее сервис WTH_GoodbyeDPI!' + [System.Environment]::NewLine + [System.Environment]::NewLine + 'Удалить?' , "WTH SCP:SL GoodbyeDPI" , [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Error)
 	if ($result -eq 'Yes') {
 		# Deleting existing GoodbyeDPI service
+		Write-Output "Останавливаем и удаляем сервис WTH_GoodbyeDPI"
 		[void](sc.exe stop "WTH_GoodbyeDPI")
 		[void](sc.exe delete "WTH_GoodbyeDPI")
 		DeleteLeftoverFiles -old_folder_path (Split-Path $path -Parent)
@@ -66,12 +72,16 @@ if ($result -eq 'Yes') {
 	[void](New-Item -Path "$path\$gdpi_folder" -ItemType Directory -Confirm:$False -Force)
 
 	# Downloading latest GoodbyeDPI to installtion folder
+	Write-Output "Скачиваем последнюю версию GoodbyeDPI"
+			
 	Invoke-RestMethod 'https://api.github.com/repos/ValdikSS/GoodbyeDPI/releases/latest' | % assets | ? name -like "*.zip" | % { 
 		Invoke-WebRequest $_.browser_download_url -OutFile ("$path\" + $_.name) 
 		$gdpi_archive_name = $_.name
 	}
 
 	# Unpack downloaded archive
+	Write-Output "Распаковываем архив"
+	
 	Expand-Archive -Path "$path\$gdpi_archive_name" -DestinationPath $path
 	$unpacked_folder = "$path\$gdpi_archive_name".TrimEnd('.zip')
 	Move-Item -Path "$unpacked_folder\*" -Destination "$path\$gdpi_folder"
@@ -80,9 +90,13 @@ if ($result -eq 'Yes') {
 	if (Test-Path "$path\$gdpi_archive_name") {[void](Remove-Item "$path\$gdpi_archive_name" -Confirm:$False -Force)}
 
 	# Download SCP:SL website list File
+	Write-Output "Скачиваем whitelist доменов SCP: Secret Laboratory"
+		
 	Start-BitsTransfer -Source 'https://raw.githubusercontent.com/REALMWTH/Powershell-GDPI-Install-Script/main/scpsl_domains.txt' -Destination "$path\$gdpi_folder"
 
 	# Install Service
+	Write-Output "Устанавливаем сервис WTH_GoodbyeDPI"
+	
 	if ($os_type -eq $True) {
 		$exe_path = [Environment]::GetEnvironmentVariable("ProgramFiles") + "\" + $gdpi_folder + "\x86_64\goodbyedpi.exe"
 	}
@@ -94,6 +108,8 @@ if ($result -eq 'Yes') {
 	[void](cmd.exe /c "sc create `"WTH_GoodbyeDPI`" binPath= `"$exe_path -5 --blacklist `"`"$path\$gdpi_folder\scpsl_domains.txt`"`"")
 	[void](sc.exe config "WTH_GoodbyeDPI" start= auto)
 	[void](sc.exe description "GoodbyeDPI" "Passive Deep Packet Inspection blocker and Active DPI circumvention utility. Affects SCP:SL Domains only.")
+	
+	Write-Output "Запускаем сервис WTH_GoodbyeDPI"
 	[void](sc.exe start "WTH_GoodbyeDPI")
 	
 	$result = [System.Windows.Forms.MessageBox]::Show('Скрипт успешно установил сервис WTH_GoodbyeDPI.' + [System.Environment]::NewLine + [System.Environment]::NewLine + "Проверьте работоспособность списка серверов SCP:SL.", "WTH SCP:SL GoodbyeDPI" , [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
