adc8209e8c60f991bd4051346bdfd975ceae7570
REALMWTH <realmbmc@mail.ru>
2024-10-09 01:05:33 +0300

    Create install_scpsl_winws.ps1

---
+++ install_scpsl_winws.ps1
@@ -0,0 +1,120 @@
+function DeleteLeftoverFiles
+{	
+	Param (
+		[string]$old_folder_path
+	)
+	
+	# Delete existing installation folder
+	if (Test-Path $old_folder_path) {
+		Write-Output "Удаляем устаревшие файлы Winws"
+		[void](Remove-Item $old_folder_path -Recurse -Confirm:$False -Force)
+	}
+}
+
+# Set title and advertisement
+
+$host.ui.RawUI.WindowTitle = "Welcome To Hell SCP:SL Winws downloader and installer"
+Write-Host "Загрузчик и инсталлятор сервиса Winws для всех доменов SCP:SL и Discord от " -ForegroundColor white -nonewline
+Write-Host "Welcome To Hell" -ForegroundColor red
+Write-Host "https://discord.scpsl.ru" -ForegroundColor white -BackgroundColor darkred
+Write-Host ""
+
+# Disable warnings and errors output
+$ErrorActionPreference = "SilentlyContinue"
+$WarningPreference = "SilentlyContinue"
+function global:Write-Host() {}
+
+# Do not prompt for confirmations
+Set-Variable -Name 'ConfirmPreference' -Value 'None' -Scope Global
+
+[void]([System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms"))
+
+# Determining Windows architecture
+$os_type = (Get-WmiObject -Class Win32_ComputerSystem).SystemType -match ‘(x64)’
+
+# Finding Winws folder
+$winws_folder = "WTH_Winws"
+$path = ""
+if ($os_type -eq $True) {
+	$path = [Environment]::GetEnvironmentVariable("ProgramFiles") + "\" + $winws_folder
+}
+else
+{
+	$path = [Environment]::GetEnvironmentVariable("ProgramFiles(x86)") + "\" + $winws_folder
+}
+
+# Checking if Winws is already installed. Ask to uninstall. If not accepted, exit script.
+Write-Output "Проверяем, установлен ли сервис WTH Winws"
+
+$winws_service_exists = Get-Service -Name "WTH Winws" -ErrorAction SilentlyContinue
+
+if ($winws_service_exists.Length -gt 0) {
+	$result = [System.Windows.Forms.MessageBox]::Show('Найден установленный ранее сервис WTH Winws!' + [System.Environment]::NewLine + [System.Environment]::NewLine + 'Удалить?' , "WTH SCP:SL Winws" , [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Error)
+	if ($result -eq 'Yes') {
+		# Deleting existing Winws service
+		Write-Output "Останавливаем и удаляем сервис WTH Winws"
+		[void](sc.exe stop "WTH Winws")
+		[void](sc.exe delete "WTH Winws")
+		DeleteLeftoverFiles -old_folder_path "$path"
+	}
+	if ($result -eq 'No') {
+		exit
+	}
+}
+
+$result = [System.Windows.Forms.MessageBox]::Show('Скрипт установит сервис WTH Winws для исправления проблемы с соединением с интернет-ресурсами игры SCP: Secret Laboratory.' + [System.Environment]::NewLine + [System.Environment]::NewLine + 'Установить?' , "WTH SCP:SL Winws" , [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)
+if ($result -eq 'Yes') {
+
+	$path = Split-Path $path -Parent
+
+	DeleteLeftoverFiles -old_folder_path "$path\$winws_folder"
+	
+	[void](New-Item -Path "$path\$winws_folder" -ItemType Directory -Confirm:$False -Force)
+
+	# Downloading latest Winws to installtion folder
+	Write-Output "Скачиваем последнюю версию Winws"
+	
+	Invoke-WebRequest -Uri "https://github.com/bol-van/zapret-win-bundle/archive/refs/heads/master.zip" -OutFile "$path\zapret-win-bundle-master.zip"
+	
+	$winws_archive_name = "zapret-win-bundle-master.zip"
+
+	# Unpack downloaded archive
+	Write-Output "Распаковываем архив"
+	
+	Expand-Archive -Path "$path\$winws_archive_name" -DestinationPath $path
+	$unpacked_folder = "$path\$winws_archive_name".TrimEnd('.zip')
+	[void](Move-Item -Path "$unpacked_folder\zapret-winws\*" -Destination "$path\$winws_folder" -Force -Confirm:$False)
+	
+	# Delete leftovers
+	if (Test-Path "$unpacked_folder") {[void](Remove-Item "$unpacked_folder" -Confirm:$False -Force -Recurse)}
+	if (Test-Path "$path\$winws_archive_name") {[void](Remove-Item "$path\$winws_archive_name" -Confirm:$False -Force)}
+	
+	# Download SCP:SL website list File
+	Write-Output "Скачиваем whitelist доменов SCP: Secret Laboratory и Discord"
+		
+	Start-BitsTransfer -Source 'https://raw.githubusercontent.com/REALMWTH/Powershell-GDPI-Install-Script/refs/heads/main/list-discord-scpsl.txt' -Destination "$path\$winws_folder"
+		
+	# Install Service
+	Write-Output "Устанавливаем сервис WTH Winws"
+	
+	if ($os_type -eq $True) {
+		$exe_path = [Environment]::GetEnvironmentVariable("ProgramFiles") + "\" + $winws_folder + "\winws.exe"
+	}
+	else
+	{
+		$exe_path = [Environment]::GetEnvironmentVariable("ProgramFiles(x86)") + "\" + $winws_folder + "\x86\goodbyedpi.exe"
+	}
+	
+	[void](cmd.exe /c "sc create `"WTH Winws`" binPath= `"$exe_path --wf-tcp=443 --wf-udp=443,50000-65535 --filter-udp=443 --hostlist=`"`"$path\$winws_folder\list-discord-scpsl.txt`"`" --dpi-desync=fake --dpi-desync-udplen-increment=10 --dpi-desync-repeats=6 --dpi-desync-udplen-pattern=0xDEADBEEF --dpi-desync-fake-quic=`"`"$path\$winws_folder\quic_initial_www_google_com.bin`"`" --new --filter-udp=50000-65535 --dpi-desync=fake,tamper --dpi-desync-any-protocol --dpi-desync-fake-quic=`"`"$path\$winws_folder\quic_initial_www_google_com.bin`"`" --new --filter-tcp=443 --hostlist=`"`"$path\$winws_folder\list-discord-scpsl.txt`"`" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=`"`"$path\$winws_folder\tls_clienthello_www_google_com.bin`"`"`"")
+	[void](sc.exe config "WTH Winws" start= auto)
+	[void](sc.exe description "WTH Winws" "Passive Deep Packet Inspection blocker and Active DPI circumvention utility. Affects SCP:SL and Discord domains only.")
+	
+	Write-Output "Запускаем сервис WTH Winws"
+	[void](sc.exe start "WTH Winws")
+	
+	$result = [System.Windows.Forms.MessageBox]::Show('Скрипт успешно установил сервис WTH Winws.' + [System.Environment]::NewLine + [System.Environment]::NewLine + "Проверьте соединение с Discord и список серверов SCP:SL.", "WTH SCP:SL Winws" , [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+	#>
+}
+if ($result -eq 'No') {
+	exit
+}
